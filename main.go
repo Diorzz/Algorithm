db9e723fbe0506fd57f86fe995df919a6eb8e305
YuaJuan <34501184+YuaJuan@users.noreply.github.com>
2019-06-25 11:59:39 +0800

    add handsome (#1)

--- main.go
+++ main.go
@@ -6,7 +6,13 @@ import (
 )
 
 func main() {
+	fmt.Println("*****************zhangzheng is handsome*******");
 	a := []int{4, 1, 2, 7, 9, 8, 0}
+	fmt.Println("*****************zhangzheng is handsome*******");
+	fmt.Println("*****************zhangzheng is handsome*******");
+	fmt.Println("*****************zhangzheng is handsome*******");
+	fmt.Println("*****************zhangzheng is handsome*******");
+	fmt.Println("*****************zhangzheng is handsome*******");
         b:=[]int{0,0}
 	sort.HeapSort(a)
 	for _, i := range a {
